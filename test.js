bcf06595195431428bd63e5358f46ecc1f1fb741
toppalyer <45781464+toppalyer@users.noreply.github.com>
2018-12-11 14:41:10 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-function test(){
-  window.location.href="http://testgamecenter.iblocke.com/";
-}
-
-function test1(){
-  alert("hello wk");
-}
+function test(){window.location.href="http://testgamecenter.iblocke.com/";}
+function test1(){alert("hello wk");}
